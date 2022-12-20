889c00d05b976005b987af5c0b2b5d1cd77ed15b
Luiz Serão <luiz.serao@hotmail.com>
2022-12-20 08:58:40 -0300

    mudando img do button-menu ao clique

--- script.js
+++ script.js
@@ -2,4 +2,5 @@ const btnMenu = document.querySelector(".button-menu")
 btnMenu.addEventListener("click", () => {
   const menu = document.querySelector(".menu-nav ul")
   menu.classList.toggle("active")
+  btnMenu.classList.toggle("active")
 })
