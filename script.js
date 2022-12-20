ef9b8b8271a209f30364ac227bd02ad35e006342
Luiz Serão <luiz.serao@hotmail.com>
2022-12-20 08:51:43 -0300

    Menu mobile em resolução de tela mobile

---
+++ script.js
@@ -0,0 +1,5 @@
+const btnMenu = document.querySelector(".button-menu")
+btnMenu.addEventListener("click", () => {
+  const menu = document.querySelector(".menu-nav ul")
+  menu.classList.toggle("active")
+})
